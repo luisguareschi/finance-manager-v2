f076c962a0708919df8f10584a8f284044df7174
luisguareschi <lg@tsl.io>
2024-08-23 16:53:00 -0400

    Added loader

--- components/DebtSummary/index.tsx
+++ components/DebtSummary/index.tsx
@@ -2,14 +2,19 @@ import useDebtSummary from "@/queries/debts/useDebtSummary";
 import DebtCard from "@/components/DebtCard";
 import dayjs from "dayjs";
 import { formatCurrency } from "@/lib/fomatCurrency";
+import Spinner from "@/components/common/spinner";
 
 const DebtSummary = () => {
   const { debtSummary, isLoading } = useDebtSummary();
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 h-full">
       <p className="text-lg font-medium text-slate-800">People who owe you:</p>
-      {isLoading && <p className="text-slate-600">Loading...</p>}
+      {isLoading && (
+        <div className="h-full flex items-center justify-center">
+          <Spinner size={36} trackColor="blue-500"/>
+        </div>
+      )}
       {debtSummary?.debtors.map((debtor) => (
         <DebtCard
           title={debtor.name}
