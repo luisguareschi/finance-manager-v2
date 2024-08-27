520b4665776c6ef730f692eed64b390a5421b179
luisguareschi <lg@tsl.io>
2024-08-26 21:55:37 -0400

    Improved select

--- components/HistoryPage/DebtorSelect.tsx
+++ components/HistoryPage/DebtorSelect.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/select";
 import { useEffect, useState } from "react";
 import useDebtors from "@/queries/debts/useDebtors";
+import { cn } from "@/lib/utils";
 
 interface props {
   debtorId: string | null;
@@ -18,6 +19,7 @@ interface props {
 const DebtorSelect = ({ debtorId, onChange }: props) => {
   const { debtors, isLoading: loadingDebtors } = useDebtors();
   const [selectedDebtor, setSelectedDebtor] = useState(debtorId || "all");
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (!onChange) {
@@ -30,13 +32,16 @@ const DebtorSelect = ({ debtorId, onChange }: props) => {
     <Select
       value={selectedDebtor}
       onValueChange={(value) => setSelectedDebtor(value)}
+      open={open}
+      onOpenChange={(value) => setOpen(value)}
     >
       <SelectTrigger
-        className="w-[180px] border-0 bg-transparent flex justify-center
-      items-center gap-2 text-lg font-medium text-slate-600 pl-4
-      focus:outline-none focus:ring-0 focus:ring-transparent focus:ring-offset-transparent
-      focus:border-2 focus:bg-slate-50 transition-colors open:bg-white
-      "
+        className={cn(
+          "w-[180px] border-0 bg-transparent flex justify-center items-center gap-2 text-lg " +
+            "font-medium text-slate-600 pl-4 focus:outline-none focus:ring-0 focus:ring-transparent " +
+            "focus:ring-offset-transparent focus:border-2 focus:bg-slate-50 transition-colors open:bg-white",
+          open ? "bg-slate-50 border-2" : "",
+        )}
       >
         <SelectValue placeholder="Debtor" />
       </SelectTrigger>
